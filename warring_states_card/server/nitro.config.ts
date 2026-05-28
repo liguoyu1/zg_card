1ac43a45c016ac16eb24fa969c1aaebaa4b280d0
guoyu <6ed17fd7-5b96-4734-94ec-4727ebd5c722-219826955590371130_qianmian@noreply.codeup.aliyun.com>
2026-05-28 18:22:27 +0800

    fix: 简化nitro配置

--- warring_states_card/server/nitro.config.ts
+++ warring_states_card/server/nitro.config.ts
@@ -1,9 +1,5 @@
 export default {
   $schema: "https://json.schemastore.org/nitro-schema.json",
   preset: "node-server",
-  server: "./server/index.ts",
-  routeRules: {
-    "/api/**": { cors: true },
-    "/ws/**": { websocket: true },
-  },
+  compatibilityDate: "2024-04-03",
 };
